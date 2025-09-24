fab80996ef00d05f1ee84f390b322195c6e8333c
shubhanshu Tiwari <149867878+shubhanshut31@users.noreply.github.com>
2025-09-24 15:12:50 +0530

    Update CityTiles.jsx

--- src/Components/CityTiles.jsx
+++ src/Components/CityTiles.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../app.css';
+import '../App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
